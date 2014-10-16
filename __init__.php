c8f599bb89210280f3bf8097c6bf483ac26e4d6e
Hyyan Abo Fakher <Tiribthea4hyyan@gmail.com>
2014-10-16 12:49:07 -0700

    Added plugin bootstrap file

---
+++ __init__.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the hyyan/slider package.
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/*
+ * Plugin Name: Hyyan Slider
+ * Plugin URI: https://github.com/hyyan/slider/
+ * Description: Wordpress plugin to create wordpress sliders using custom post types and taxonomies
+ * Author: Hyyan Abo Fakher
+ * Version: 0.1
+ * Author URI: https://github.com/hyyan
+ * GitHub Plugin URI: hyyan/slider
+ * Domain Path: /languages
+ * Text Domain: hyyan-slider
+ * License: MIT License
+ */
+
+if (!defined('ABSPATH'))
+    exit('restricted access');
+
